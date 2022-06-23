df45098293b47e9cd48aad1c3259894c0b07495e
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2022-06-23 20:13:00 +0800

    fix: has only 1 menu set custom theme

--- app/index.jsx
+++ app/index.jsx
@@ -1,26 +1,18 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback } from "react";
 import { TopbarMenuItem } from "@nbundle/react";
+import { AiOutlineFormatPainter } from "react-icons/ai";
 
 export default function App() {
-  const showTwitterPreview = useCallback(() => {
-    console.log("Show Twitter preview");
-  });
-  const [enableCustomTheme, setEnableCustomTheme] = useState(false);
-  useEffect(() => {
-    console.log("Custom theme: ", enableCustomTheme ? "Enabled" : "Disabled");
-  }, [enableCustomTheme]);
+  const setCustomTheme = useCallback(() => {
+    console.log("Select custom theme");
+  }, []);
   return (
     <>
       <TopbarMenuItem
         type="button"
-        text="Show Twitter preview"
-        onClick={showTwitterPreview}
-      />
-      <TopbarMenuItem
-        type="toggle"
-        text="Custom theme"
-        value={enableCustomTheme}
-        onChange={setEnableCustomTheme}
+        text="Set custom theme"
+        icon={AiOutlineFormatPainter}
+        onClick={setCustomTheme}
       />
     </>
   );
