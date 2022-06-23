5b8c11034ec6ab7baafaea625faa5d5ba47977ca
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2022-06-23 11:15:52 +0800

    fix: render topbar menu items

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,25 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { TopbarMenuButtonItem, TopbarMenuToggleItem } from "@nbundle/react";
 
 export default function App() {
+  const showTwitterPreview = useCallback(() => {
+    console.log("Show Twitter preview");
+  });
+  const [enableCustomTheme, setEnableCustomTheme] = useState(false);
   useEffect(() => {
-    console.log("nbundle example: loaded");
-    return () => console.log("nbundle example: unloaded");
-  }, []);
-  return <></>;
+    console.log("Custom theme: ", enableCustomTheme ? "Enabled" : "Disabled");
+  }, [enableCustomTheme]);
+  return (
+    <>
+      <TopbarMenuButtonItem
+        text="Show Twitter preview"
+        onClick={showTwitterPreview}
+      />
+      <TopbarMenuToggleItem
+        text="Custom theme"
+        value={enableCustomTheme}
+        onChange={setEnableCustomTheme}
+      />
+    </>
+  );
 }
